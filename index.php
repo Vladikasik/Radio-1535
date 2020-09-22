d3c1021ec670e626fdb6c2ac74e798e55bfd6e0d
Vlad <vsakayafignavlad@gmail.com>
2020-09-23 00:46:45 +0300

    Footer + links

---
+++ index.php
@@ -0,0 +1,38 @@
+<meta charset="utf-8">
+
+<html>
+
+	<head>
+
+		<title>Radio 1535</title>
+
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<script type="text/javascript" src="clock.js"></script>
+
+	</head>
+
+	<body onload="startTime()">
+
+		<div class="topnav">
+			<div id='menu-clock'><div id="txt"></div></div>
+  			<a class="active" href="index.php">Слушать</a>
+  			<a href="podcast.html">Подкасты</a>
+  			<a href="schedule.html">Расписание</a>
+  			<a href="memes.html">Мемы</a>
+  			<a href="vote.html">Голосовалка</a>
+		</div>
+
+		<div id="main-container">
+
+				<h1>Play music</h1>
+
+				<a href="#" class="round-button"><i class="fa fa-play fa-2x"></i></a>
+		</div>
+
+	<?php
+		include("footer.php");
+	?>
+
+	</body>
+
+</html>
